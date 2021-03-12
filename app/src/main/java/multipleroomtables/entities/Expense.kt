f9b3a65122b03c65af4d2587eedd728e18fa567d
Karl <kosimox@web.de>
2021-03-12 17:47:08 +0100

    removed unused classes and fixed an entity.

--- app/src/main/java/multipleroomtables/entities/Expense.kt
+++ app/src/main/java/multipleroomtables/entities/Expense.kt
@@ -1,9 +1,13 @@
 package multipleroomtables.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity(primaryKeys = ["person_id", "group_id"])
+@Entity
 data class Expense (
+    @PrimaryKey(autoGenerate = true)
+    val expense_id: Int,
+
     val person_id: Int,
     val group_id: Int
 )
